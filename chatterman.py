c07609104161c22047eee27b35216dacfcc37997
tcoy <trevor8_@hotmail.com>
2023-06-29 21:37:16 -0400

    weight responses, generate left

--- chatterman.py
+++ chatterman.py
@@ -5,31 +5,111 @@ import random
 import nltk
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.corpus import stopwords
-from nltk.corpus import names as nltknames
+from nltk.corpus import names
 
 memory = {}
 phrase_step = 1
 
-def _get_phrases_of_interest(str):
-	phrases = []
-	sents = sent_tokenize(str)
+def _is_name(word):
+	return word.title() in names.words('male.txt') or word.title() in names.words('female.txt')
 
-	for sent in sents:
-		words = [word for word in word_tokenize(
-			re.sub('[' + string.punctuation + ']', '', sent)) if word.lower() not in stopwords.words('english')]
+def _interest_words(str):
+	words = set(word_tokenize(str.lower()))
+	stops = set(stopwords.words('english'))
+	punc = set([*string.punctuation])
+	return words - stops - punc
 
-		for word in words:
-			for phrase in memory:
-				token = memory[phrase]
-				if word in token['related_words'] and phrase not in phrases:
-					phrases.append(phrase)
+def _interest_tokens(str):
+	interest_words = _interest_words(str)
+	interest_tokens = {}
+	for word in interest_words:
+		for k, v in memory.items():
+			if word in k.split():
+				interest_tokens[k] = v
+	return interest_tokens
 
-	return phrases
+def _weighted_choice(tokens):
+	if tokens:
+		weights = [v['weight'] for v in tokens.values()]
+		keys = [*tokens.keys()]
+		if any(w > 0 for w in weights):
+			return random.choices(keys, weights = weights, k = 1)[0]
+		else:
+			return random.choice(keys)
+	return None
+
+def _token_left(response):
+	words = response.split()
+	left = words[0] # get the first word in the phrase
+	related = {}
+	for k, v in memory.items():
+		if left in v['related_words']:
+			related[k] = v
+	while (choice := _weighted_choice(related)) is not None and choice in response:
+		related.pop(choice) # remove any related tokens already included in the response
+	if choice is not None:
+		return choice.split()[-1]
+	return None
+
+def _token_right(response):
+	words = response.split()
+	right = ' '.join(words[-phrase_step:])
+	token = memory.get(right)
+	if token:
+		related = token['related_words']
+		choices = []
+		if len(related) > 0:
+			weights = []
+			for w in related:
+				words.append(w) # prepare the word to test
+				next_phrase = ' '.join(words[-phrase_step:])
+				if next_phrase not in response:
+					if w not in choices:
+						choices.append(w )
+						weights.append(0)
+					if (next_token := memory.get(next_phrase)) is not None:
+						weights[choices.index(w)] += next_token['weight']
+					else:
+						weights[choices.index(w)] += token['weight']				
+				words.pop()
+			if any(w > 0 for w in weights):
+				choice = random.choices(choices, weights = weights, k = 1)[0]
+				if choice is not None:
+					print(choice + ' : ' + str(weights[choices.index(choice)]))
+				else:
+					print('None : ' + str(weights[choices.index(choice)]))
+				return choice
+			elif len(choices) > 0: # possible all related words were popped
+				choice = random.choice(choices)
+				if choice is not None:
+					print(choice + ' : random')
+				else:
+					print('None : random')
+				return choice
+	return None
+
+def _pick_start_token(str):
+	interest_tokens = _interest_tokens(str)
+	population = interest_tokens if len(interest_tokens) > 0 else memory # use interest tokens if avaiable, else use anything from memory
+	return _weighted_choice(population)
+	
+def _generate_response(str):
+	phrase = _pick_start_token(str)
+	print('start: ' + phrase)
+	response = phrase
+
+	while (right := _token_right(response)) is not None:
+		response = response + ' ' + right # append left
+		if (left := _token_left(response)) is not None:
+			response = left + ' ' + response
+
+	return response
 
 def _has_corpa():
 	try:
 		nltk.find('tokenizers/punkt')
 		nltk.find('corpora/stopwords')
+		nltk.find('corpora/names')
 		return True
 	except LookupError:
 		return False
@@ -38,13 +118,12 @@ def read(str):
 	sents = sent_tokenize(str.lower())
 
 	for sent in sents:
-		words = word_tokenize(
-			re.sub('[' + string.punctuation + ']', '', sent))
+		words = word_tokenize(sent)
 
 		if len(words) <= phrase_step:
 			continue
 
-		for i in range(0, len(words)):
+		for i in range(0, len(words)): 
 			phrase = ' '.join(words[i:i + phrase_step:1])
 			related_word = None
 
@@ -54,54 +133,30 @@ def read(str):
 				break
 
 			token = memory.get(phrase)
-
+			weight = 4 if any(_is_name(w) for w in phrase.split()) else 1
 			if token == None:
 				token = {}
-				token['weight'] = 0
+				token['weight'] = weight
 				token['related_words'] = []
 			else:
-				token['weight'] += 1
+				token['weight'] += weight
 
-			token['related_words'].append(related_word)
+			if related_word not in token['related_words']:
+				token['related_words'].append(related_word)
 			memory[phrase] = token
 	
 def reply(str):
 	if not _has_corpa():
-		return '\U0001F648' # error!
+		return '🙈' # error!
 	
 	str = str.lower()
 	has_memory = len(memory) > 0
-	response = '\U0001F64A' # no response
+	response = '🙊' # no response
 	
 	if has_memory:
-		phrases_of_interest = _get_phrases_of_interest(str)
-		names = [name for name in phrases_of_interest if name.title() in nltknames.words('male.txt') or name.title() in nltknames.words('female.txt')]
-
-		if len(names) > 0:
-			start_phrase = random.choice(names)
-		else:
-			start_phrase = random.choice(list(phrases_of_interest)) if len(
-				phrases_of_interest) > 0 else random.choice(list(memory))
-			
-		current_phrase = start_phrase
-		current_token = memory[current_phrase]
-		response = current_phrase
-		used_phrases = [current_phrase]
-		while len(current_token['related_words']) > 0:
-			next_word = random.choice(current_token['related_words'])
-			response += ' ' + next_word
-			full_phrase = word_tokenize(current_phrase)
-			full_phrase.append(next_word)
-			next_phrase = ' '.join(full_phrase[len(full_phrase) - phrase_step::])
-
-			if next_phrase not in memory or next_phrase in used_phrases:
-				break
-
-			current_phrase = next_phrase
-			current_token = memory[current_phrase]
-			used_phrases.append(current_phrase)
+		response = _generate_response(str)
+		response = re.sub(' \'', '\'', response)
 		if random.randint(0, 10)==10: response += ' ' + random.choice(['🥵','😊','🥺','🙈','🙉','🙊','😈','😏','😄','🤢','😬'])
-
 
 	read(str)
 
--- cli.py
+++ cli.py
@@ -5,31 +5,14 @@ import chatterman
 CONFIG_FILENAME = 'config.json'
 MEMORY_FILENAME = 'memory.json'
 
-def load_config():
-	global config
-	try:
-		with open(CONFIG_FILENAME) as config_file:
-			config = json.load(config_file)
-		return True
-	except FileNotFoundError:
-		return False
-	
 def load_memory():
-	with open(MEMORY_FILENAME, 'w+') as memory_file:
+	with open(MEMORY_FILENAME, 'r') as memory_file:
 		try:
 			return json.load(memory_file)
 		except json.JSONDecodeError:
 			return {}
 
-def save_memory():
-	with open(MEMORY_FILENAME, 'w+') as memory_file:
-			memory_file.write(json.dumps(chatterman.memory))
-			print("Memory saved")
-
-if not load_config():
-	sys.exit("Failed to load cli.py config!")
-
-chatterman.phrase_step = config.get('phrase_step', 1)
+chatterman.phrase_step = 2
 chatterman.memory = load_memory()
 
 while True:
